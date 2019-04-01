0dc169434b4b54be887457bf205cc2f03399aa6f
EasySimpleCool <easysimplecool@gmail.com>
2019-04-01 14:11:01 +1100

    build

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -2,9 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import { interopDefault } from './utils'
 
-const _65d17980 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
-const _119979aa = () => interopDefault(import('../pages/_logos.vue' /* webpackChunkName: "pages/_logos" */))
 const _bd7b20d4 = () => interopDefault(import('../pages/logos.vue' /* webpackChunkName: "pages/logos" */))
+const _65d17980 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 Vue.use(Router)
 
@@ -81,17 +80,13 @@ export function createRouter() {
     scrollBehavior,
 
     routes: [{
+      path: "/logos",
+      component: _bd7b20d4,
+      name: "logos"
+    }, {
       path: "/",
       component: _65d17980,
       name: "index"
-    }, {
-      path: "/:logos",
-      component: _119979aa,
-      children: [{
-        path: "",
-        component: _bd7b20d4,
-        name: "logos"
-      }]
     }],
 
     fallback: false
